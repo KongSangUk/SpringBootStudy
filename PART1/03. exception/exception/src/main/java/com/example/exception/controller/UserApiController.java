025bb1daeb25adf78116b8cd30b8468125aa520f
Kong Sang Uk <qkqhfp19@naver.com>
2023-07-01 12:48:39 +0900

    exception

---
+++ PART1/03. exception/exception/src/main/java/com/example/exception/controller/UserApiController.java
@@ -0,0 +1,65 @@
+package com.example.exception.controller;
+
+
+import com.example.exception.model.Api;
+import com.example.exception.model.UserResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserApiController {
+
+    private static List<UserResponse> userList = List.of(
+            UserResponse.builder()
+                    .id("1")
+                    .age(27)
+                    .name("공상욱")
+                    .build()
+            ,
+            UserResponse.builder()
+                    .id("2")
+                    .age(23)
+                    .name("곰상욱")
+                    .build()
+
+    );
+
+    @GetMapping("/id/{userId}")
+    public Api<UserResponse> getUser(@PathVariable String userId) {
+
+        if (true){
+            throw new RuntimeException("message");
+        }
+
+        var user = userList.stream()
+                .filter(it -> it.getId().equals(userId))
+                .findFirst()
+                .get();
+
+
+        Api<UserResponse> response = Api.<UserResponse>builder()
+                .resultCode(String.valueOf(HttpStatus.OK.value()))
+                .resultMessage(HttpStatus.OK.name())
+                .data(user)
+                .build();
+
+        return response;
+    }
+
+    /*{
+        "result_code":null,
+            "result_message":null,
+            "data":{
+        "id":"1",
+                "name":"공상욱",
+                "age":27
+    }
+    }*/
+
+}
